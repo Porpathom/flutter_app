64b51e4518d4f7177335760798c80be7a2c1666e
Porpathom <117138205+Porpathom@users.noreply.github.com>
2024-07-03 11:05:32 +0700

    backup before start

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -2,50 +2,103 @@ import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
- 
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    
+
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
-        title: Text(widget.title),
+        title: const Text("Hello, My Friend"),
       ),
-      body: Center(
-        child: Column(
+    body: const Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: Center(
+              child: Text(
+                "Strefmgrgnerg;lergmlenr",
+                style:TextStyle(fontSize: 24),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "Row and Column are basic primitive widgets for horizontal and vertical layouts—these low-level widgets allow for maximum customization. Flutter also offers specialized, higher level widgets that might be sufficient for your needs. For example, instead of Row you might prefer ListTile, an easy-to-use widget with properties for leading and trailing icons, and up to 3 lines of text. Instead of Column, you might prefer ListView, a column-like layout that automatically scrolls if its content is too long to fit the available space. For more information, see Common layout widgets.",
+              style: TextStyle(fontSize: 18),
+            ),
+          ),
+        Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                  Icon(Icons.star),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Row(
+              children: [
+                Text(
+                  "170 Reviews",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+
+        SizedBox(
+          height: 40,
+        ),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                 Icon(Icons.kitchen),
+                 Text('PREP:'),
+                 Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer),
+                Text('COOK:'),
+                Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant),
+                Text('FEEDS:'),
+                Text('4-6'),
+              ],
+            ),
+          ],
+        )
+        ],
+        ),
+    ),
     );
-  }
+  } 
 }
